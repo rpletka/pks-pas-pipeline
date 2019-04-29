5ff45e824424d7292366564f9d2104febc5eaabd
Niran Even-chen <38479071+NiranEC77@users.noreply.github.com>
2019-04-29 17:05:36 -0400

    Update upload_stemcell.sh

--- functions/upload_stemcell.sh
+++ functions/upload_stemcell.sh
@@ -1,9 +1,85 @@
 #!/bin/bash
 
 function upload_stemcells() (
-om-linux -t https://pas-opsman.corp.local -u admin -p VMware1! -k upload-stemcell -s 170.48
-  
 
-  
+  set -eu
+  local stemcell_os=$1
+  local stemcell_versions="$2"
+
+  for stemcell_version_reqd in $stemcell_versions
+  do
+
+    if [ -n "$stemcell_version_reqd" ]; then
+      diagnostic_report=$(
+        om-linux \
+          --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+          --username $OPSMAN_USERNAME \
+          --password $OPSMAN_PASSWORD \
+          --skip-ssl-validation \
+          curl --silent --path "/api/v0/diagnostic_report"
+      )
+
+      stemcell=$(
+        echo $diagnostic_report |
+        jq \
+          --arg version "$stemcell_version_reqd" \
+          --arg glob "$IAAS" \
+        '.stemcells[] | select(contains($version) and contains($glob))'
+      )
+
+      if [[ -z "$stemcell" ]]; then
+        echo "Downloading stemcell $stemcell_version_reqd"
+
+        product_slug=$(
+          jq --raw-output \
+            '
+            if any(.Dependencies[]; select(.Release.Product.Name | contains("Stemcells for PCF (Windows)"))) then
+              "stemcells-windows-server"
+            else
+              "stemcells"
+            end
+            ' < pivnet-product/metadata.json
+        )
+
+        pivnet-cli login --api-token="$PIVNET_API_TOKEN"
+    set +e
+        # Override the product_slug for xenial
+        if [[ "$stemcell_os" =~ "trusty" ]]; then
+          product_slug="stemcells"
+        elif [[ "$stemcell_os" =~ "xenial" ]]; then
+          product_slug="stemcells-ubuntu-xenial"
+        fi
+
+        pivnet-cli download-product-files -p "$product_slug" -r $stemcell_version_reqd -g "*${IAAS}*" --accept-eula
+        if [ $? != 0 ]; then
+          min_version=$(echo $stemcell_version_reqd | awk -F '.' '{print $2}')
+          major_version=$(echo $stemcell_version_reqd | awk -F '.' '{print $1}')
+          if [ "$min_version" == "" ]; then
+            for min_version in $(seq 100 -1 0)
+            do
+               pivnet-cli download-product-files -p "$product_slug" -r $major_version.$min_version -g "*${IAAS}*" --accept-eula && break
+            done
+          else
+            echo "No Stemcell version $major_version for "${IAAS}" found !!, giving up"
+            exit 1
+          fi
+        fi
+    set -e
+
+        SC_FILE_PATH=`find ./ -name "bosh*.tgz" | sort | tail -1 || true`
+
+        if [ ! -f "$SC_FILE_PATH" ]; then
+          echo "Stemcell file not found!"
+          exit 1
+        fi
+
+        for stemcell in $SC_FILE_PATH
+        do
+          om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD -k upload-stemcell -s $stemcell
+        done
+      fi
+    fi
+
+  done
 
 )
